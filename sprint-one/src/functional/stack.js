43a47c9d310d779d979556bcca1befa0c0c08992
MKS-Student <mks.student@hackreactor.com>
2016-06-01 16:14:50 -0700

    Completed functional qeue and stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,27 @@ var Stack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var size = 0;
   // Implement the methods below
   someInstance.push = function(value){
+    storage[size] = value;
+    size++;
   };
 
   someInstance.pop = function(){
+    if (size === 0) {
+      return null;
+    } 
+
+    size--;
+
+    var tempResult = storage[size];
+    delete storage[size];
+    return tempResult;
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
