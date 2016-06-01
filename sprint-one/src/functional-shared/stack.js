f65b67d1dc5e341024f8be09df2defbf25a2bc7f
MKS-Student <mks.student@hackreactor.com>
2016-06-01 16:45:54 -0700

    worked on stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,51 @@
 var Stack = function() {
+	var someInstance = {};
+	var storage = {};
+	var size = 0;
+	_.extend(someInstance, stackMethods);
+	return someInstance;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
-var stackMethods = {};
+	var stackMethods = {};
+
+	stackMethods.push = function(value) {
+		this.storage[this.size] = value;
+		this.size++;
+	};
+
+	stackMethods.pop = function() {
+		if(this.size === 0) {
+			return null;
+		}
+		this.size--;
+		var tempResult = this.storage[this.size];
+		delete this.storage[this.size];
+		return tempResult;
+	};
+
+	stackMethods.size = function() {
+		return this.size;
+	};
+
+// var stackMethods = {
+// 	push : function(value) {
+// 		this.storage[this.size] = value;
+// 		this.size++;
+// 	},
+// 	pop : function() {
+// 		if(this.size === 0) {
+// 			return null;
+// 		}
+// 		this.size--;
+// 		var tempResult = this.storage[this.size];
+// 		delete this.storage[this.size];
+// 		return tempResult;
+// 	},
+// 	size : function() {
+// 		return this.size;
+// 	}
+// };
 
 
