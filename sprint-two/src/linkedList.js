4545d9a54ae465861f4b3d185695ea5c65858a77
MKS-Student <mks.student@hackreactor.com>
2016-06-03 16:35:59 -0700

    Completed linkedlist and tree

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,27 +3,53 @@ var LinkedList = function(){
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(value){
+  list.addToTail = function(value) {
+    var newNode = Node(value);
+    if(list.head === null) {
+      list.head = newNode;
+    }
+    if(list.head.next === null){
+      list.tail = newNode;
+    }
+    list.tail.next = newNode;
+    list.tail = newNode;
   };
 
   list.removeHead = function(){
+    var tempNode = list.head;
+    list.head = list.head.next;
+    return tempNode.value;
   };
 
   list.contains = function(target){
+    var found = false;
+    var traversal = function(node){
+      if(node.value === target){
+        found = true;
+      }
+      if(node.next !== null){
+        traversal(node.next);
+      }
+    };
+    traversal(list.head);
+    return found;
   };
 
   return list;
 };
 
-var Node = function(value){
-  var node = {};
 
-  node.value = value;
-  node.next = null;
 
-  return node;
-};
 
+  var Node = function(value) {
+    var node = {};
+    node.value = value;
+    node.next = null;
+
+    return node;
+  };
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,11 @@
-var Tree = function(value){
+var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
+  // extend newTree with treeMethods.
+  _.extend(newTree, treeMethods);
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
 
   return newTree;
 };
@@ -15,11 +17,24 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  var node = Tree(value);
+  this.children.push(node);
 };
 
-treeMethods.contains = function(target){
-
+treeMethods.contains = function(target) {
+  var found = false;
+  var traversal = function(node) {
+  	_.each(node, function(current) {
+  	  if (current.value === target) {
+  	  	found = true;
+  	  }
+  	  if (current.children) {
+  	  	traversal(current.children);
+  	  }
+  	});
+  };
+  traversal(this.children);
+  return found;
 };
 
 
